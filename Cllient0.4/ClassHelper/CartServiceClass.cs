9ea5434cb819457db74b92cbf0d38f92e37074c4
KoshelevaOlga <126570872+KoshelevaOlga@users.noreply.github.com>
2023-04-17 14:25:22 +0300

    Add files via upload

---
+++ Cllient0.4/ClassHelper/CartServiceClass.cs
@@ -0,0 +1,14 @@
+﻿using Cllient0._4.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cllient0._4.ClassHelper
+{
+    internal class CartServiceClass
+    {
+        public static List<Service> ServiceCart { get; set; } = new List<Service>();
+    }
+}
